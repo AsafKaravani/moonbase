880de2ac0aba6a569ebb0ee05527cebedcbd638f
Asaf <asaf.karavani1@gmail.com>
2022-02-17 22:51:08 +0200

    .

--- .semaphore/get-npm-package-version.sh
+++ .semaphore/get-npm-package-version.sh
@@ -1,10 +1,6 @@
 # Version key/value should be on his own line
 echo cheking $1 for a version
-PACKAGE_VERSION=$(cat $1 \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+PACKAGE_VERSION=0.0.0
 
 export PACKAGE_VERSION
 echo $PACKAGE_VERSION
