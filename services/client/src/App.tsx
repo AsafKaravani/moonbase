a6dfd693fadff79d379350fb7baad4fb6ad8811f
Asaf <asaf.karavani1@gmail.com>
2022-03-16 02:31:00 +0200

    remove test cicd code

--- services/client/src/App.tsx
+++ services/client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count} test 4
+            count is: {count}
           </button>
         </p>
         <p>
